48e3363e21eee12053bd416f62a379ab15dbc333
Erick Andres Vergara Noriega <erickandres30@gmail.com>
2020-06-24 22:46:43 -0500

    Creating components to User logged or not logged and change the component when the user is logged

--- app/navigations/AccountStack.js
+++ app/navigations/AccountStack.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
 // Components
-import Account from "../screens/Account";
+import Account from "../screens/Account/Account";
 
 const Stack = createStackNavigator();
 
