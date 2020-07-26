ad650893c92b32dc71d8bb35d1bdb821193ed004
Erick Vergara <erickandres30@gmail.com>
2020-07-25 22:56:24 -0500

    Creacion y ajuste de estilos del boton para crear restaurantes en la screen de Restaurants

--- app/screens/Restaurants/Restaurants.js
+++ app/screens/Restaurants/Restaurants.js
@@ -1,10 +1,35 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { Icon } from "react-native-elements";
+import { colors } from "../../theme/colors";
 
 export default function Restaurants() {
-  return (
-    <View>
-      <Text>Restaurans....</Text>
-    </View>
-  );
+    return (
+        <View style={styles.viewBody}>
+            <Text>Restaurans....</Text>
+
+            <Icon
+                reverse
+                type="material-community"
+                name="plus"
+                color={colors.green}
+                containerStyle={styles.btnContainer}
+            />
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    viewBody: {
+        flex: 1,
+        backgroundColor: colors.white,
+    },
+    btnContainer: {
+        position: "absolute",
+        bottom: 10,
+        right: 10,
+        shadowColor: "black",
+        shadowOffset: { width: 2, height: 2 },
+        shadowOpacity: 0.5,
+    },
+});
